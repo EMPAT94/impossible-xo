2d05db1c533e9aff8d3aae6b3befacf2c7ba96e7
Pritesh Tupe <priteshtupe@gmail.com>
2021-04-28 21:56:37 +0530

    Full ai logic; some beautification; make playable

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ A tic-tac-toe game that is impossible to win (but possible to lose!)
 
 ## Project Objectives
 
-1. Serve as a learning sample for Jest & Cypress testing frameworks
-2. Be a small "how-to" on tic-tac-toe AI logic (in general, any board game)
-3. Have fun!
+1. Be a small "how-to" on tic-tac-toe logic that I use to be undefeatable!
+2. Have fun!
 
 ## Build
 
--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 const NEW_BOARD = ["", "", "", "", "", "", "", "", ""];
-const x = "x";
-const o = "o";
+const x = "X";
+const o = "O";
 const d = "don't care";
 const e = "empty";
 
@@ -15,40 +15,82 @@ const state = {
 const compareBoards = (b1, b2) => b1.every((c, i) => (c === e ? !b2[i] : c === d || c === b2[i]));
 
 function checkIfWinningOnNextTurn(b, s) {
-  if (compareBoards([e, s, s], b)) return "0";
-  if (compareBoards([s, e, s], b)) return "1";
-  if (compareBoards([s, s, e], b)) return "2";
+  /**
+   * Note:
+   * d = don't care
+   * e = empty
+   * s = x | o
+   *
+   * s wins if e becomes s on next turn
+   */
+
+  const d_r = [d, d, d];
+  const r_1 = [e, s, s];
+  const r_2 = [s, e, s];
+  const r_3 = [s, s, e];
+
+  // Win first row
+  if (compareBoards(r_1, b)) return "0";
+  if (compareBoards(r_2, b)) return "1";
+  if (compareBoards(r_3, b)) return "2";
+
+  // Win second row
+  if (compareBoards([...d_r, ...r_1], b)) return "3";
+  if (compareBoards([...d_r, ...r_2], b)) return "4";
+  if (compareBoards([...d_r, ...r_3], b)) return "5";
+
+  // Win third row
+  if (compareBoards([...d_r, ...d_r, ...r_1], b)) return "6";
+  if (compareBoards([...d_r, ...d_r, ...r_2], b)) return "7";
+  if (compareBoards([...d_r, ...d_r, ...r_3], b)) return "8";
+
+  // Win first column
+  if (compareBoards([e, d, d, s, d, d, s, d, d], b)) return "0";
+  if (compareBoards([s, d, d, e, d, d, s, d, d], b)) return "3";
+  if (compareBoards([s, d, d, s, d, d, e, d, d], b)) return "6";
+
+  // Win second column
+  if (compareBoards([d, e, d, d, s, d, d, s, d], b)) return "1";
+  if (compareBoards([d, s, d, d, e, d, d, s, d], b)) return "4";
+  if (compareBoards([d, s, d, d, s, d, e, e, d], b)) return "7";
+
+  // Win third column
+  if (compareBoards([d, d, e, d, d, s, d, d, s], b)) return "2";
+  if (compareBoards([d, d, s, d, d, e, d, d, s], b)) return "5";
+  if (compareBoards([d, d, s, d, d, s, d, d, e], b)) return "8";
+
+  // Win diagonal left up to right down
+  if (compareBoards([e, d, d, d, s, d, d, d, s], b)) return "0";
+  if (compareBoards([s, d, d, d, e, d, d, d, s], b)) return "4";
+  if (compareBoards([s, d, d, d, s, d, d, d, e], b)) return "8";
+
+  // Win diagonal left down to right up
+  if (compareBoards([d, d, e, d, s, d, s, d, d], b)) return "2";
+  if (compareBoards([d, d, s, d, e, d, s, d, d], b)) return "4";
+  if (compareBoards([d, d, s, d, s, d, e, d, d], b)) return "6";
 }
 
 // Main Logic of next play
 function aiPlay() {
   const board = state.board;
-  switch (state.plTurns) {
-    case 1:
+  // Check for self wins, snatch win if present
+  let i = checkIfWinningOnNextTurn(board, state.next);
+  if (i) board[+i] = state.next;
+  else {
+    // If no self win, check for opponent win, block that win
+    i = checkIfWinningOnNextTurn(board, state.prev);
+    if (i) board[+i] = state.next;
+    else {
       // If center if free, take it
       if (!board[4]) board[4] = state.next;
       // Else take a corner on top
       else if (!board[0]) board[0] = state.next;
-      else if (!board[2]) board[2] = state.next;
-      break;
-    case 2:
-      // Check for opponent win, block that win
-      let i = checkIfWinningOnNextTurn(board, state.prev);
-      if (i) board[+i] = state.next;
-      // Take some other cell
       else {
-        if (!board[1]) board[1] = state.next;
-        else if (!board[7]) board[7] = state.next;
-        else if (!board[3]) board[3] = state.next;
+        // Pick the first empty spot
+        i = board.findIndex((c) => !c);
+        board[+i] = state.next;
       }
-      break;
-    case 3:
-    // Check for self wins, snatch win if present
-    // If no self win, check for opponent win, block that win
-    // If no opponent win, try for self win
-   case 4:
-    default:
-      break;
+    }
   }
 
   state.aiTurns++;
@@ -73,7 +115,10 @@ function setState(state) {
 
 function setBoard(board) {
   for (let i = 0; i < board.length; i++) {
-    if (board[i]) dom_cells[i].innerHTML = board[i];
+    if (board[i] && dom_cells[i].innerHTML != board[i]) {
+      dom_cells[i].innerHTML = board[i];
+      dom_cells[i].className = board[i] === x ? "cell-x" : "cell-o";
+    }
   }
 }
 
